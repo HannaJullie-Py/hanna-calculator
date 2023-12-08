b8e21a7a176b097511267015f98b6191a92f1822
mtyzlam <114540964+mtyzlam@users.noreply.github.com>
2023-12-08 15:36:37 -0300

    Update 2_Creditos.py

--- pages/2_Creditos.py
+++ pages/2_Creditos.py
@@ -5,6 +5,7 @@ st.markdown("# Créditos")
 st.title('Quem Fez?')
 
 st.write('Feito por Hanna Patrocinio Benevides da Silveira e Pedro Henrique Soares de Souza!')
-st.write('Todos os direitos reservados. ANAPH © 2023')
 
 st.image('https://lists.office.com/Images/9188040d-6c67-4c5b-b112-36a304b66dad/00000000-0000-0000-0003-7ffe6d30f122/TCRLG2XGWMUL46YDNFB17BNCLL/d4de046e-3c39-436b-aa0d-b1a5e01f378d')
+
+st.write('Todos os direitos reservados. ANAPH © 2023')
