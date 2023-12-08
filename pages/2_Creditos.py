effc0801098f64c21480f3926bf4dfd123dd1d1b
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-08 15:21:57 -0300

    Update 2_Creditos.py

--- pages/2_Creditos.py
+++ pages/2_Creditos.py
@@ -2,4 +2,8 @@ import streamlit as st
 
 st.set_page_config(page_title="Créditos", page_icon="💯")
 st.markdown("# Créditos")
+st.title('Quem Fez?')
 
+st.write('Feito por Hanna Patrocinio Benevides da Silveira e Pedro Henrique Soares de Souza!')
+
+st.image('https://lists.office.com/Images/9188040d-6c67-4c5b-b112-36a304b66dad/00000000-0000-0000-0003-7ffe6d30f122/TCRLG2XGWMUL46YDNFB17BNCLL/d4de046e-3c39-436b-aa0d-b1a5e01f378d')
