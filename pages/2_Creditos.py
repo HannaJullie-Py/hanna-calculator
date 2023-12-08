c7aefca84cfd091b15ae5b9742e1926430a52664
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-08 15:40:02 -0300

    Update 2_Creditos.py

--- pages/2_Creditos.py
+++ pages/2_Creditos.py
@@ -9,4 +9,6 @@ st.divider()
 
 st.image('https://lists.office.com/Images/9188040d-6c67-4c5b-b112-36a304b66dad/00000000-0000-0000-0003-7ffe6d30f122/TCRLG2XGWMUL46YDNFB17BNCLL/d4de046e-3c39-436b-aa0d-b1a5e01f378d')
 
+st.divider()
+
 st.write('Todos os direitos reservados. ANAPH © 2023')
