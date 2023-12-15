d887de3957f4f6a1f9520e58bfa79899542d3de3
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-15 14:34:24 -0300

    Add files via upload

---
+++ Inicio.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import time
+from streamlit_chat import message
+
+
+#Título
+st.set_page_config(page_title="ANAPH",page_icon="💡")
+st.header('ANAPH', divider='rainbow')
+st.image('https://i.imgur.com/1P4Ggm3.png',width=700)
+st.write('Analisamos o seu consumo de energia elétrica!')
+
+#Explicação
+st.title('Como funciona?')
+st.write(' **Aqui no site da ANAPH é mais fácil calcular o seu consumo de energia, podendo filtrar na data desejada, quando e onde quiser! Crie sua conta no menu CADASTRO para ter acesso completo às funcionalidades do site. Agradecemos a sua visita.** ')
