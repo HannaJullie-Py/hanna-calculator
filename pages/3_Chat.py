38b7f3e348cf2eeef804abb11d5ed75d54e869f5
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-15 14:40:19 -0300

    Update 3_Chat.py

--- pages/3_Chat.py
+++ pages/3_Chat.py
@@ -3,6 +3,18 @@ import numpy as np
 
 import streamlit as st
 
+usu = st.text_input('Crie seu nome de usuário')
+pas = st.text_input('Crie sua senha')
+pas1 = st.text_input('Confirme sua senha')
+
+
+butao = st.button('Enter em cada uma das caixas para criar')
+if pas1 != pas:
+  st.caption(':red[Senha não confere]')
+
+if butao:
+  st.caption(':green[Conta criada :D]')
+
 prompt = st.chat_input("Say something")
 if prompt:
-    st.write(f"User has sent the following prompt: {prompt}")
+    st.write(f"{usu} enviou: {prompt}")
