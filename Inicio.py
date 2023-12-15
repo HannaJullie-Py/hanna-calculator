0d726272a0c6b3cc0cea734db95bdab41340e99d
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-15 15:16:34 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -4,9 +4,73 @@ import numpy as np
 import time
 
 
+def dialog_close_button_clicked():
+    st.write("###  Dialog close callback results:")
+    st.write("#### No preferences saved!")
+    dialog.close()
 
-#Título
-st.set_page_config(page_title="ANAPH",page_icon="💡")
+dialog = st.dialog(
+    "first_dialog_with_help", title="Introduce yourself",
+    on_close_button_clicked=dialog_close_button_clicked, can_be_closed=True)
+
+def dialog_form_submit_button_clicked():
+    st.write("### Dialog form submit button clicked results:")
+    st.write(f"#### Hey {st.session_state.first_name} {st.session_state.last_name}!")
+    st.write(f"#### Your preferences are saved")
+    dialog.close()
+
+
+with dialog:
+    st.text_input("First name", key="first_name")
+    st.text_input("Last name", key="last_name")
+    st.checkbox("I accept cookies", key="cookies")
+    st.form_submit_button("OK", on_click=dialog_form_submit_button_clicked)
+
+
+if st.button("Open dialog", key="first_dialog_button"):
+    dialog.open()def dialog_close_button_clicked():
+    st.write("###  Dialog close callback results:")
+    st.write("#### No preferences saved!")
+    dialog.close()
+
+dialog = st.dialog(
+    "first_dialog_with_help", title="Introduce yourself",
+    on_close_button_clicked=dialog_close_button_clicked, can_be_closed=True)
+
+def dialog_form_submit_button_clicked():
+    st.write("### Dialog form submit button clicked results:")
+    st.write(f"#### Hey {st.session_state.first_name} {st.session_state.last_name}!")
+    st.write(f"#### Your preferences are saved")
+    dialog.close()
+
+
+with dialog:
+    st.text_input("First name", key="first_name")
+    st.text_input("Last name", key="last_name")
+    st.checkbox("I accept cookies", key="cookies")
+    st.form_submit_button("OK", on_click=dialog_form_submit_button_clicked)
+
+
+if st.button("Open dialog", key="first_dialog_button"):
+    dialog.open()    "primeiro_popup", title="Crie sua conta!",
+    on_close_button_clicked=botao_fechar_popup, can_be_closed=True)
+
+def dialog_form_submit_button_clicked():
+    st.write("### Dialog form submit button clicked results:")
+    st.write(f"#### Hey {st.session_state.first_name} {st.session_state.last_name}!")
+    st.write(f"#### Your preferences are saved")
+    dialog.close()
+
+
+with dialog:
+    st.text_input("First name", key="first_name")
+    st.text_input("Last name", key="last_name")
+    st.checkbox("I accept cookies", key="cookies")
+    st.form_submit_button("OK", on_click=dialog_form_submit_button_clicked)
+
+
+if st.button("Open dialog", key="first_dialog_button"):
+    dialog.open()
 st.header('ANAPH', divider='rainbow')
 st.image('https://i.imgur.com/1P4Ggm3.png',width=700)
 st.write('Analisamos o seu consumo de energia elétrica!')
