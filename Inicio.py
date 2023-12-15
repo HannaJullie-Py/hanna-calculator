1564e0ec940c3392f642d530dfafeaca2ff1c1ce
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-15 15:31:31 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -3,6 +3,36 @@ import pandas as pd
 import numpy as np
 import time
 st.set_page_config('ANAPH', page_icon='⚡')
+
+def dialog_botao_fechar():
+    st.write("###  Resultado do PopUp fechado:")
+    st.write("#### Nada à salvar!")
+    popup.close()
+
+popup = st.dialog(
+    "primeiro_popup", title="Crie sua conta1",
+    on_botao_fechar_clicked=dialog_botao_fechar, pode_fechar=True)
+
+def dialog_botao_criar():
+    st.write("### Resultado do Criar no PopUp:")
+    st.write(f"#### Eai {st.session_state.usuario})
+    st.write(f"#### Conta Criada")
+    popup.close()
+
+
+with popup:
+    usu = st.text_input("Usuário", key="usuario")
+    pas = st.text_input("Senha", key="senha")
+    pas1 = st.text_input("Confirme sua senha", key= "confirme")
+    if pas1 != pas:
+      st.caption(':red[Senha não confere]')
+    st.checkbox("Eu aceito cookies!", key="cookies")
+    st.form_submit_button("Criar", on_click=dialog_botao_criar)
+
+
+if st.button("Open dialog", key="first_dialog_button"):
+    dialog.open()
+
 st.header('ANAPH', divider='rainbow')
 st.image('https://i.imgur.com/1P4Ggm3.png',width=700)
 st.write('Analisamos o seu consumo de energia elétrica!')
