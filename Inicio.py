dee46a1e367fe9103b3688b4fda4ed32eb99178b
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2023-12-15 15:34:17 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -7,9 +7,9 @@ st.set_page_config('ANAPH', page_icon='⚡')
 def dialog_botao_fechar():
     st.write("###  Resultado do PopUp fechado:")
     st.write("#### Nada à salvar!")
-    popup.close()
+    dialog.close()
 
-popup = st.dialog(
+dialog = st.dialog(
     "primeiro_popup", title="Crie sua conta",
     on_botao_fechar_clicked=dialog_botao_fechar, pode_fechar=True)
 
@@ -17,10 +17,10 @@ def dialog_botao_criar():
     st.write("### Resultado do Criar no PopUp:")
     st.write(f"#### Eai {st.session_state.usuario}")
     st.write(f"#### Conta Criada")
-    popup.close()
+    dialog.close()
 
 
-with popup:
+with dialog:
     usu = st.text_input("Usuário", key="usuario")
     pas = st.text_input("Senha", key="senha")
     pas1 = st.text_input("Confirme sua senha", key= "confirme")
@@ -29,9 +29,7 @@ with popup:
     st.checkbox("Eu aceito cookies!", key="cookies")
     st.form_submit_button("Criar", on_click=dialog_botao_criar)
 
-
-if st.button("Open dialog", key="first_dialog_button"):
-    dialog.open()
+dialog.open()
 
 st.header('ANAPH', divider='rainbow')
 st.image('https://i.imgur.com/1P4Ggm3.png',width=700)
