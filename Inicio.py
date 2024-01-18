3f6ae80710b83291347005e48f44d6e4fae555dc
HannaJullie.Py <152525312+HannaJullie-Py@users.noreply.github.com>
2024-01-18 14:32:52 -0300

    Update Inicio.py

--- Inicio.py
+++ Inicio.py
@@ -7,16 +7,19 @@ st.header('ANAPH', divider='rainbow')
 st.image('https://i.imgur.com/1P4Ggm3.png',width=700)
 st.write('Analisamos o seu consumo de energia elétrica!')
 
-page_bg_img = '''
-<style>
-body {
-background-image: url("https://static.mundoeducacao.uol.com.br/mundoeducacao/2023/06/barragem-de-uma-usina-hidreletrica.jpg");
-background-size: cover;
-}
-</style>
-'''
-
-st.markdown(page_bg_img, unsafe_allow_html=True)
+st.markdown(
+    """
+    <style>
+    .reportview-container {
+        background: url("https://static.mundoeducacao.uol.com.br/mundoeducacao/2023/06/barragem-de-uma-usina-hidreletrica.jpg")
+    }
+   .sidebar .sidebar-content {
+        background: url("https://static.mundoeducacao.uol.com.br/mundoeducacao/2023/06/barragem-de-uma-usina-hidreletrica.jpg")
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 
 #Explicação
 st.title('Como funciona?')
